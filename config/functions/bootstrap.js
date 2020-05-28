9c7d28476bbf117d82419639b16f6818942d01ca
Vera Scheunert <vera.scheunert@aol.com>
2020-05-28 10:49:43 +0200

    Set default permissions on startup

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -10,6 +10,12 @@
  * See more details here: https://strapi.io/documentation/3.0.0-beta.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {
-  console.log ('HEEEEELLLOOO!!!!');
+module.exports = async () => {
+  const authenticated = await strapi.query('role', 'users-permissions').findOne({ type: 'authenticated' });
+  authenticated.permissions.filter(permission => permission.type == 'application' && !permission.enabled).forEach(permission => {
+    strapi.query('permission', 'users-permissions').update( { id: permission.id}, {
+      ...permission,
+      enabled: true
+    });
+  });
 };
